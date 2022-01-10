26f89089e122af36e3cb5d76137bbc5477ee16ff
Nikita Shulga <nshulga@fb.com>
2022-01-10 09:13:33 -0800

    Add dispatched workflow
    (cherry picked from commit 0bcdc2db1bd83bbcb5a085a12ea0367b363a4cf3)

---
+++ .github/scripts/trymerge.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+from urllib import parse
+import os
+import json
+
+if __name__ == "__main__":
+    token = os.environ.get("GITHUB_TOKEN")
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    if token is not None:
+        headers['Authorization'] = f'token {token}'
+    data=json.dumps({"body":f"body " + " ".join(sys.argv)}).encode()
+    req = Request("https://api.github.com/repos/malfet/deleteme/issues/1/comments", headers=headers, data=data, method='POST')
+    with urlopen(req) as reply:
+        print(reply.read())
