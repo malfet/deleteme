60525cb2e6216d4ad7e8cbc0dad510c59a6e4c77
Nikita Shulga <nshulga@fb.com>
2022-01-11 10:50:13 -0800

    Infer repo owner/name
    (cherry picked from commit 947e6101f0584d7e82f8cccd4b740f0554d5abca)

--- .github/scripts/trymerge.py
+++ .github/scripts/trymerge.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import json
+import re
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
-from typing import Any, Callable, Dict, List, Optional
+from typing import cast, Any, Callable, Dict, List, Optional, Tuple
 
 
-GH_GET_PR_INFO_QUERY="""
+GH_GET_PR_INFO_QUERY = """
 query ($owner: String!, $name: String!, $number: Int!) {
   repository(owner: $owner, name: $name) {
     pullRequest(number: $number) {
@@ -37,6 +38,35 @@ query ($owner: String!, $name: String!, $number: Int!) {
   }
 }
 """
+
+GH_REMOTE_URL_MATCH = re.compile("^https://.*@?github.com/(.+)/(.+)$")
+
+
+def _check_output(items: List[str], encoding: str = "utf-8") -> str:
+    from subprocess import check_output
+    return check_output(items).decode(encoding)
+
+
+def get_git_remote_name() -> str:
+    import os
+    return os.getenv("GIT_REMOTE_NAME", "origin")
+
+
+def get_git_repo_dir() -> str:
+    import os
+    from pathlib import Path
+    return os.getenv("GIT_REPO_DIR", str(Path(__file__).resolve().parent.parent))
+
+
+def gh_get_repo_owner_and_name() -> Tuple[str, str]:
+    remote_name = get_git_remote_name()
+    repo_dir = get_git_repo_dir()
+    url = _check_output(["git", "-C", repo_dir, "remote", "get-url", remote_name])
+    rc = GH_REMOTE_URL_MATCH.match(url)
+    if rc is None:
+        raise RuntimeError(f"Unexpected url format {url}")
+    return cast(Tuple[str, str], rc.groups())
+
 
 def _fetch_url(url: str, *,
                headers: Optional[Dict[str, str]] = None,
@@ -71,15 +101,15 @@ def fetch_json(url: str,
 
 def gh_post_comment(org: str, project: str, pr_num: int, comment: str) -> List[Dict[str, Any]]:
     return fetch_json(f'https://api.github.com/repos/{org}/{project}/issues/{pr_num}/comments',
-                      data= {"body": comment})
+                      data={"body": comment})
 
 
 def gh_update_pr(org: str, project: str, pr_num: int, *,
-                      base: Optional[str] = None,
-                      state: Optional[str] = None,
-                      title: Optional[str] = None,
-                      body: Optional[str] = None,
-                      ) -> List[Dict[str, Any]]:
+                 base: Optional[str] = None,
+                 state: Optional[str] = None,
+                 title: Optional[str] = None,
+                 body: Optional[str] = None,
+                 ) -> List[Dict[str, Any]]:
     data = {}
     if base is not None:
         data["base"] = base
@@ -96,30 +126,31 @@ def gh_update_pr(org: str, project: str, pr_num: int, *,
                       reader=json.load)
 
 
-
 def gh_graphql(query: str, **kwargs: Any) -> List[Dict[str, Any]]:
-    return _fetch_url("https://api.github.com/graphql", data={"query":query, "variables": kwargs}, reader=json.load)
+    return _fetch_url("https://api.github.com/graphql", data={"query": query, "variables": kwargs}, reader=json.load)
 
 
 def gh_get_pr_info(org: str, proj: str, pr_no: int) -> Any:
     rc = gh_graphql(GH_GET_PR_INFO_QUERY, name=proj, owner=org, number=pr_no)
     return rc["data"]["repository"]["pullRequest"]
 
-def gh_fetch_pr_diff(org: str , proj: str, pr_no: int) -> str:
+
+def gh_fetch_pr_diff(org: str, proj: str, pr_no: int) -> str:
     headers = {'Accept': 'application/vnd.github.v3.diff'}
     return _fetch_url(f'https://api.github.com/repos/{org}/{proj}/pulls/{pr_no}', headers).decode("utf-8")
 
+
 def main() -> None:
     import sys
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print("Unexpected number of arguments")
         sys.exit(-1)
 
-    org, project = sys.argv[1].split('/', 1)
-    pr_num = int(sys.argv[2])
+    org, project = gh_get_repo_owner_and_name()
+    pr_num = int(sys.argv[1])
 
     info = gh_get_pr_info(org, project, pr_num)
-    if int(info["changedFiles"])>100:
+    if int(info["changedFiles"]) > 100:
         gh_post_comment(org, project, pr_num, "Can't merge: too many files changed")
         sys.exit(-1)
 
