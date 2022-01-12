4e94d8c8c1655f3767b33e825f3cd8ffbee90494
Nikita Shulga <nshulga@fb.com>
2022-01-11 16:29:33 -0800

    More changes

--- .github/scripts/trymerge.py
+++ .github/scripts/trymerge.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+import os
 import re
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
@@ -11,21 +12,40 @@ GH_GET_PR_INFO_QUERY = """
 query ($owner: String!, $name: String!, $number: Int!) {
   repository(owner: $owner, name: $name) {
     pullRequest(number: $number) {
-      state,
-      title,
-      body,
+      closed
+      isCrossRepository
+      author {
+        login
+      }
+      title
+      body
       headRefName
       headRepository {
         nameWithOwner
       }
       baseRefName
+      commits(first: 100) {
+        nodes {
+          commit {
+            author {
+              user {
+                login
+              }
+              email
+              name
+            }
+            oid
+          }
+        }
+        totalCount
+      }
       changedFiles,
       files(last: 100) {
         nodes {
-          path,
+          path
         }
       }
-      reviews(last: 100) {
+      latestReviews(last: 100) {
         nodes {
           author {
             login
@@ -39,7 +59,8 @@ query ($owner: String!, $name: String!, $number: Int!) {
 }
 """
 
-GH_REMOTE_URL_MATCH = re.compile("^https://.*@?github.com/(.+)/(.+)$")
+RE_GITHUB_URL_MATCH = re.compile("^https://.*@?github.com/(.+)/(.+)$")
+RE_GHSTACK_HEAD_REF = re.compile(r"^(gh/[^/]+/[0-9]+/)head$")
 
 
 def _check_output(items: List[str], encoding: str = "utf-8") -> str:
@@ -48,24 +69,40 @@ def _check_output(items: List[str], encoding: str = "utf-8") -> str:
 
 
 def get_git_remote_name() -> str:
-    import os
     return os.getenv("GIT_REMOTE_NAME", "origin")
 
 
 def get_git_repo_dir() -> str:
-    import os
     from pathlib import Path
     return os.getenv("GIT_REPO_DIR", str(Path(__file__).resolve().parent.parent))
 
 
-def gh_get_repo_owner_and_name() -> Tuple[str, str]:
-    remote_name = get_git_remote_name()
-    repo_dir = get_git_repo_dir()
-    url = _check_output(["git", "-C", repo_dir, "remote", "get-url", remote_name])
-    rc = GH_REMOTE_URL_MATCH.match(url)
-    if rc is None:
-        raise RuntimeError(f"Unexpected url format {url}")
-    return cast(Tuple[str, str], rc.groups())
+class GitRepo:
+    def __init__(self, path: str, remote: str = "origin") -> None:
+        self.repo_dir = path
+        self.remote = remote
+
+    def _run_git(self, *args: Any) -> str:
+        return _check_output(["git", "-C", self.repo_dir] + list(args))
+
+    def revlist(self, revision_range: str) -> List[str]:
+        rc = self._run_git("rev-list", revision_range, "--", ".").strip()
+        return rc.split("\n") if len(rc) > 0 else []
+
+    def current_branch(self) -> str:
+        return self._run_git("symbolic-ref", "--short", "HEAD").strip()
+
+    def remote_url(self) -> str:
+        return self._run_git("remote", "get-url", self.remote)
+
+    def gh_owner_and_name(self) -> Tuple[str, str]:
+        url = os.getenv("GIT_REMOTE_URL", None)
+        if url is None:
+            url = self.remote_url()
+        rc = RE_GITHUB_URL_MATCH.match(url)
+        if rc is None:
+            raise RuntimeError(f"Unexpected url format {url}")
+        return cast(Tuple[str, str], rc.groups())
 
 
 def _fetch_url(url: str, *,
@@ -73,16 +110,14 @@ def _fetch_url(url: str, *,
                data: Optional[Dict[str, Any]] = None,
                method: Optional[str] = None,
                reader: Callable = lambda x: x.read()) -> Any:
-    import os
     if headers is None:
         headers = {}
     token = os.environ.get("GITHUB_TOKEN")
     if token is not None and url.startswith('https://api.github.com/'):
         headers['Authorization'] = f'token {token}'
-    if data:
-        data = json.dumps(data).encode()
+    data_ = json.dumps(data).encode() if data is not None else None
     try:
-        with urlopen(Request(url, headers=headers, data=data, method=method)) as conn:
+        with urlopen(Request(url, headers=headers, data=data_, method=method)) as conn:
             return reader(conn)
     except HTTPError as err:
         if err.code == 403 and all(key in err.headers for key in ['X-RateLimit-Limit', 'X-RateLimit-Used']):
@@ -99,7 +134,10 @@ def fetch_json(url: str,
     return _fetch_url(url, headers=headers, data=data, reader=json.load)
 
 
-def gh_post_comment(org: str, project: str, pr_num: int, comment: str) -> List[Dict[str, Any]]:
+def gh_post_comment(org: str, project: str, pr_num: int, comment: str, dry_run: bool = False) -> List[Dict[str, Any]]:
+    if dry_run:
+        print(comment)
+        return []
     return fetch_json(f'https://api.github.com/repos/{org}/{project}/issues/{pr_num}/comments',
                       data={"body": comment})
 
@@ -126,7 +164,7 @@ def gh_update_pr(org: str, project: str, pr_num: int, *,
                       reader=json.load)
 
 
-def gh_graphql(query: str, **kwargs: Any) -> List[Dict[str, Any]]:
+def gh_graphql(query: str, **kwargs: Any) -> Dict[str, Any]:
     return _fetch_url("https://api.github.com/graphql", data={"query": query, "variables": kwargs}, reader=json.load)
 
 
@@ -137,25 +175,121 @@ def gh_get_pr_info(org: str, proj: str, pr_no: int) -> Any:
 
 def gh_fetch_pr_diff(org: str, proj: str, pr_no: int) -> str:
     headers = {'Accept': 'application/vnd.github.v3.diff'}
-    return _fetch_url(f'https://api.github.com/repos/{org}/{proj}/pulls/{pr_no}', headers).decode("utf-8")
+    return _fetch_url(f'https://api.github.com/repos/{org}/{proj}/pulls/{pr_no}', headers=headers).decode("utf-8")
+
+
+def parse_args() -> Any:
+    from argparse import ArgumentParser
+    parser = ArgumentParser("Merge PR into default branch")
+    parser.add_argument("--dry-run", action="store_true")
+    parser.add_argument("pr_num", type=int)
+    return parser.parse_args()
+
+
+class GitHubPR:
+    def __init__(self, org: str, project: str, pr_num: int) -> None:
+        self.org = org
+        self.project = project
+        self.pr_num = pr_num
+        self.info = gh_get_pr_info(org, project, pr_num)
+
+    def is_closed(self) -> bool:
+        return bool(self.info["closed"])
+
+    def is_cross_repo(self) -> bool:
+        return bool(self.info["isCrossRepository"])
+
+    def get_base_ref(self) -> str:
+        return self.info["baseRefName"]
+
+    def get_head_ref(self) -> str:
+        return self.info["headRefName"]
+
+    def is_ghstack_pr(self) -> bool:
+        return RE_GHSTACK_HEAD_REF.match(self.get_head_ref()) is not None
+
+    def get_changed_files_count(self) -> int:
+        return int(self.info["changedFiles"])
+
+    def get_changed_files(self) -> List[str]:
+        rc = [x["path"] for x in self.info["files"]["nodes"]]
+        if len(rc) != self.get_changed_files_count():
+            raise RuntimeError("Changed file count mismatch")
+        return rc
+
+    def _get_reviewers(self) -> List[Tuple[str, bool]]:
+        reviews_count = int(self.info["latestReviews"]["totalCount"])
+        if len(self.info["latestReviews"]["nodes"]) != reviews_count:
+            raise RuntimeError("Can't fetch all PR reviews")
+        return [(x["author"]["login"], x["state"]) for x in self.info["latestReviews"]["nodes"]]
+
+    def get_approved_by(self) -> List[str]:
+        return [login for (login, state) in self._get_reviewers() if state == "APPROVED"]
+
+    def get_commit_count(self) -> int:
+        return int(self.info["commits"]["totalCount"])
+
+    def get_pr_creator_login(self) -> str:
+        return self.info["author"]["login"]
+
+    def get_committer_login(self, num: int = 0) -> str:
+        return self.info["commits"]["nodes"][num]["commit"]["author"]["user"]["login"]
+
+    def get_committer_author(self, num: int = 0) -> str:
+        node = self.info["commits"]["nodes"][num]["commit"]["author"]
+        return f"{node['name']} <{node['email']}>"
+
+    def get_authors(self) -> Dict[str, str]:
+        rc = {}
+        for idx in range(self.get_commit_count()):
+            rc[self.get_committer_login(idx)] = self.get_committer_author(idx)
+
+        return rc
+
+    def get_author(self) -> str:
+        authors = self.get_authors()
+        if len(authors) == 1:
+            return next(iter(authors.values()))
+        return self.get_authors()[self.get_pr_creator_login()]
+
+    def get_title(self) -> str:
+        return self.info["title"]
+
+    def get_body(self) -> str:
+        return self.info["body"]
+
+    def get_pr_url(self) -> str:
+        return f"https://github.com/{self.org}/{self.project}/pull/{self.pr_num}"
+
+    def merge_pr(self, repo: GitRepo, dry_run: bool = False) -> None:
+        current_branch = repo.current_branch()
+        if not self.is_ghstack_pr():
+            repo._run_git("merge", "--squash", f"{repo.remote}/{self.get_head_ref}")
+            repo._run_git("commit", f"--author=\"{self.get_author()}\"", "-m", self.get_body())
+        # For ghstack, cherry-pick commits based from origin
+        orig_ref = f"{repo.remote}/{self.get_head_ref().rsplit('/', 1)[0]}/orig"
 
 
 def main() -> None:
     import sys
-    if len(sys.argv) != 2:
-        print("Unexpected number of arguments")
+    args = parse_args()
+    repo = GitRepo(get_git_repo_dir(), get_git_remote_name())
+    org, project = repo.gh_owner_and_name()
+    current_branch = repo.current_branch()
+
+    pr = GitHubPR(org, project, args.pr_num)
+    if pr.is_closed():
+        print(gh_post_comment(org, project, args.pr_num, f"Can't merge closed PR #{args.pr_num}", dry_run=args.dry_run))
         sys.exit(-1)
 
-    org, project = gh_get_repo_owner_and_name()
-    pr_num = int(sys.argv[1])
-
-    info = gh_get_pr_info(org, project, pr_num)
-    if int(info["changedFiles"]) > 100:
-        gh_post_comment(org, project, pr_num, "Can't merge: too many files changed")
+    if pr.is_cross_repo():
+        print(gh_post_comment(org, project, args.pr_num, "Cross-repo merges are not supported at the moment", dry_run=args.dry_run))
         sys.exit(-1)
 
-    files_changed = [x["path"] for x in info["files"]["nodes"]]
-    print(gh_post_comment(org, project, pr_num, f"argv={sys.argv}\nfiles_changed={files_changed}"))
+    changed_files = pr.get_changed_files()
+    approved_by = pr.get_approved_by()
+    author = pr.get_author()
+    print(gh_post_comment(org, project, args.pr_num, f"args={args}\nfiles_changed={changed_files}\nreviewers={approved_by}\nauthor={author}", dry_run=args.dry_run))
 
 
 if __name__ == "__main__":
