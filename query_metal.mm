5ba295d08b1a6ac348be5fce580a33b8dd339623
Nikita Shulga <nshulga@fb.com>
2022-05-23 11:25:14 -0700

    Query number of devices

--- query_metal.mm
+++ query_metal.mm
@@ -3,7 +3,8 @@
 int main() {
   @autoreleasepool {
      NSArray* devices = [MTLCopyAllDevices() autorelease];
-     for (unsigned long i = 0 ; i < [devices count] ; i++) {
+     NSLog(@"Metal device count %lu", devices.count);
+     for (unsigned long i = 0 ; i < devices.count ; i++) {
         id<MTLDevice>  device = devices[i];
         NSLog(@"Found device %@ isLowPower %s", device.name, device.isLowPower ? "true" : "false");
      }
