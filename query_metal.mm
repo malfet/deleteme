3e2a03a2530fbaf12381217f13cd7735b22bde82
Nikita Shulga <nshulga@fb.com>
2022-06-22 17:10:10 -0700

    Update base for Update on "GHStack commit 3"
    This is interesting
    
    [ghstack-poisoned]

--- query_metal.mm
+++ query_metal.mm
@@ -1,5 +1,4 @@
 #include <Metal/Metal.h>
-#include <MetalPerformanceShadersGraph/MetalPerformanceShadersGraph.h>
 
 int main() {
   @autoreleasepool {
@@ -9,15 +8,5 @@ int main() {
         id<MTLDevice>  device = devices[i];
         NSLog(@"Found device %@ isLowPower %s", device.name, device.isLowPower ? "true" : "false");
      }
-
-    if (@available(macOS 12.3, *)) {
-       NSLog(@"MacOS available");
-    }
-
-    if(@available(macOSApplicationExtension 12.3, *)) {
-      NSLog(@"Extension available");
-    }
-    NSOperatingSystemVersion ver = [[NSProcessInfo processInfo] operatingSystemVersion];
-    NSLog(@"OS version %ld.%ld.%ld\n", ver.majorVersion, ver.minorVersion, ver.patchVersion);
   }
 }
