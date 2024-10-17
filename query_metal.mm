3f14b3c14db7c85ce8900e0d2c20bb052adbb5bb
Nikita Shulga <nshulga@meta.com>
2024-10-17 09:35:19 -0700

    Improve query metal
    To actually try to alloc memory

--- query_metal.mm
+++ query_metal.mm
@@ -1,15 +1,38 @@
 #include <Metal/Metal.h>
 
+#include <stdexcept>
+#include <string>
+
+id<MTLDevice> getMetalDevice() {
+  NSArray *devices = [MTLCopyAllDevices() autorelease];
+  NSLog(@"Metal device count %lu", devices.count);
+  if (devices.count == 0) {
+    throw std::runtime_error("Metal is not supported");
+  }
+  // Print some info
+  for (unsigned long i = 0 ; i < devices.count ; i++) {
+    id<MTLDevice>  device = devices[i];
+    NSLog(@"Found device %@ isLowPower %s supports Metal %s",
+          device.name,
+          device.isLowPower ? "true" : "false",
+          [device supportsFamily:MTLGPUFamilyMac2] ? "true" : "false");
+  }
+  return devices[0];
+}
+
+id<MTLBuffer> allocSharedBuffer(id<MTLDevice> device, unsigned length) {
+  id<MTLBuffer> rc = [device newBufferWithLength:length
+                                         options:MTLResourceStorageModeShared];
+  if (rc == nil) {
+    throw std::runtime_error("Can't allocate " + std::to_string(length) +
+                             " bytes on GPU");
+  }
+  return rc;
+}
+
 int main() {
   @autoreleasepool {
-     NSArray* devices = [MTLCopyAllDevices() autorelease];
-     NSLog(@"Metal device count %lu", devices.count);
-     for (unsigned long i = 0 ; i < devices.count ; i++) {
-        id<MTLDevice>  device = devices[i];
-        NSLog(@"Found device %@ isLowPower %s supports Metal %s",
-              device.name,
-              device.isLowPower ? "true" : "false",
-              [device supportsFamily:MTLGPUFamilyMac2] ? "true" : "false");
-     }
+    id<MTLDevice> dev = getMetalDevice();
+    auto buf_C = allocSharedBuffer(dev, 1024 * 1024);
   }
 }
